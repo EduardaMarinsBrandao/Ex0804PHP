9fdaea1e64e20481a1a7d4790ff7977bb69a457e
EduardaMarinsBrandao <duda13brandao@gmail.com>
2025-04-10 07:44:25 -0300

    códigos calcular.php

--- Desafio/calcular.php
+++ Desafio/calcular.php
@@ -6,45 +6,31 @@
 </head>
 <body>
 <?php
-
-// Declara as variáveis e captura os dados do formulário
-$chute = $_POST['chute'] ?? ''; 
-$numeroAleatorio = $_POST['sorte']; 
-$qtdTentativas = $_POST['qtdTentativas'] ?? 0;
-$qtdTentativas++;
-
-// Verifica se o chute está correto
-if ($chute == $numeroAleatorio) 
-{
-    // Caso acerte
-    echo "<h2>Parabéns! Você acertou o número da sorte: $numeroAleatorio</h2>";
-    echo "<p>Você precisou de $qtdTentativas tentativa(s) para acertar.</p>";
-    echo "<p>Obrigado por jogar!</p>";
-} else 
-{
-    // Caso não acerte, verifica se o chute foi menor ou maior e exibe o feedback
-    if ($chute < $numeroAleatorio) 
-    {
-        echo "<p>Seu chute foi menor que o número da sorte.</p>";
-    } else if ($chute > $numeroAleatorio) 
-    {
-        echo "<p>Seu chute foi maior que o número da sorte.</p>";
-    }
-
-    echo $numeroAleatorio;
- 
-
-    // Recarrega o formulário com os valores atualizados
-    echo "<form method='post' action='calcular.php'>";
-    echo "<input type='hidden' name='chute' value='$chute'>";
-    echo "<input type='hidden' name='sorte' value='$numeroAleatorio'>"; 
-    echo "<input type='hidden' name='qtdTentativas' value='$qtdTentativas'>"; 
-
-    echo "</form>";
-}
-
-
+    $numeroAleatorio = $_POST['sorte'];
+    $tentativas = $_POST['tentativas'] + 1;
+    $numeroUsuario = $_POST['numero'];
 ?>
-<p><a href="index.php">Tentar Novamente</a></p>
+<form action="calcular.php" method="post">
+    <input type="hidden" name="sorte" value="<?php echo $numeroAleatorio; ?>">
+    <input type="hidden" name="tentativas" value="<?php echo $tentativas; ?>">
+
+    <?php
+        if ($numeroUsuario == $numeroAleatorio) 
+		{
+            header("Location: numero.php?sorte=$numeroAleatorio&tentativas=$tentativas");
+            exit;
+        } 
+		else if ($numeroUsuario < $numeroAleatorio) 
+		{
+            echo "<p>O número é maior. Tente novamente:</p>";
+        } 
+		else 
+		{
+            echo "<p>O número é menor. Tente novamente:</p>";
+        }
+    ?>
+    <input type="number" name="numero" required>
+    <button type="submit">Enviar</button>
+</form>
 </body>
 </html>
